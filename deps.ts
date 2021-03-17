76366f053629d1f4bfe6b4e937d1297c92494420
playerx <ez@jok.io>
2021-03-17 10:44:54 +0400

    feat: initial configuration

---
+++ deps.ts
@@ -0,0 +1,9 @@
+export {
+  MemoryTransport,
+  createTransportApi,
+  createTransportHandler,
+} from 'https://raw.githubusercontent.com/playerx/deno-transport/master/mod.ts'
+
+export type { ApiWithExecutableKeys } from 'https://raw.githubusercontent.com/playerx/deno-transport/master/mod.ts'
+
+export { NatsTransport } from 'https://raw.githubusercontent.com/playerx/deno-transport-nats/master/mod.ts'
